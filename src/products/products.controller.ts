c6e3027ef2f231120f937986707e331c6c21eaea
EricMI <98968734+EricMI-Dapper@users.noreply.github.com>
2025-10-21 16:14:50 -0600

    feat: add pagination and custom exception handling to products controller

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,8 +1,14 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Delete, Get, Inject, Param, Patch, Post, Query } from '@nestjs/common';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
+import { firstValueFrom } from 'rxjs';
+import { PaginationDto } from 'src/common';
+import { PRODUCT_SERVICE } from 'src/config';
 
 @Controller('products')
 export class ProductsController {
-  constructor() { }
+  constructor(
+    @Inject(PRODUCT_SERVICE) private readonly productsClient: ClientProxy
+  ) { }
 
   @Post()
   createProduct() {
@@ -10,13 +16,22 @@ export class ProductsController {
   }
 
   @Get()
-  findAllProducts() {
-    return 'This action returns all products';
+  findAllProducts(@Query() PaginationDto: PaginationDto) {
+    return this.productsClient.send({ cmd: 'find_all_products' }, PaginationDto);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return `This action returns a #${id} product`;
+  async findOne(@Param('id') id: number) {
+    try {
+      const product = await firstValueFrom(
+        this.productsClient.send({ cmd: 'find_one_product' }, { id })
+      );
+
+      return product;
+
+    } catch (error) {
+      throw new RpcException(error);
+    }
   }
 
   @Delete(':id')
