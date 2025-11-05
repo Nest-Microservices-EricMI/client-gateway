e6935351ac0dc34857191dc773decd6fff5b5bdb
EricMI <98968734+EricMI-Dapper@users.noreply.github.com>
2025-11-05 08:01:59 -0600

    feat: update console log message in main.ts for clarity

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,7 @@ async function bootstrap() {
 
   await app.listen(envs.port);
 
-  console.log('Hola mundo - Primer cambio');
+  console.log('Hola mundo - Segundo cambio');
 
   logger.log(`Gateway running on port ${envs.port}`);
 }
