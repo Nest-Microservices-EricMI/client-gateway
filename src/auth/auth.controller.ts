b71a4986b608b699f253e749864e3dad7e31cce6
EricMI <98968734+EricMI-Dapper@users.noreply.github.com>
2025-11-26 12:35:16 -0600

    feat: implement authentication guard and decorators for user and token retrieval; enhance verify endpoint in auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,11 @@
-import { Body, Controller, Get, Inject, Post } from '@nestjs/common';
+import { Body, Controller, Get, Inject, Post, Req, UseGuards } from '@nestjs/common';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { NATS_SERVICE } from 'src/config';
 import { LoginUserDto, RegisterUserDto } from './dto';
 import { catchError } from 'rxjs';
+import { AuthGuard } from './guards/auth.guard';
+import { Token, User } from './decorators';
+import { CurrentUser } from './interfaces/current-user.interface';
 
 @Controller('auth')
 export class AuthController {
@@ -28,9 +31,10 @@ export class AuthController {
     );
   }
 
+  @UseGuards(AuthGuard)
   @Get('verify')
-  verify() {
-    return this.client.send('auth.verify.user', {})
+  verifyToken(@User() user: CurrentUser, @Token() token: string) {
+    return { user, token };
   }
 
 }
